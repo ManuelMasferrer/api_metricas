2085411f49ee74efad914005e040a6c7072607e0
Juan Camilo Daza <jc.dazam1@uniandes.edu.co>
2022-09-09 13:45:08 -0500

    module cultura-producto

--- src/cultura-producto/cultura-producto.module.ts
+++ src/cultura-producto/cultura-producto.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CulturaGastronomicaEntity } from '../culturagastronomica/culturagastronomica.entity';
+import { ProductoEntity } from '../producto/producto.entity';
 import { CulturaProductoService } from './cultura-producto.service';
 
 @Module({
-  providers: [CulturaProductoService]
+  imports: [TypeOrmModule.forFeature([CulturaGastronomicaEntity, ProductoEntity])],
+  controllers: [],
+  providers: [
+    CulturaProductoService,],
 })
+
+export class CulturaRestauranteModule {}
 export class CulturaProductoModule {}
