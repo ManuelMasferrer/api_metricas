b8825085057b435ce71b31981aafbe5b80a2246f
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2022-11-02 00:50:40 -0600

    modified app.controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it.only('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
