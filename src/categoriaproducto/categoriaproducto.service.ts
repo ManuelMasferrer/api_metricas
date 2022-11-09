8563fd7fbe6c6b7644a152943bec1ce3fc4d0483
farojasp1 <fa.rojasp1@uniandes.edu.co>
2022-11-08 23:44:11 -0500

    fix code smell catproducto

--- src/categoriaproducto/categoriaproducto.service.ts
+++ src/categoriaproducto/categoriaproducto.service.ts
@@ -1,13 +1,8 @@
 
 import { Injectable } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriaproductoEntity } from './categoriaproducto.entity';
-import { faker } from '@faker-js/faker';
-import { RegionEntity } from '../region/region.entity';
 import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
 
 
