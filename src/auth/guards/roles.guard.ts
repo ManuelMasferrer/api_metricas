633141878e295aba9b88cb688083e0056ece4449
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2022-11-09 18:00:37 -0600

    fixed code smells in roles and ciudadÂ

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,9 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from 'src/usuario/role.enum';
-// import { ROLES_KEY } from 'src/usuario/roles.decorator';
-import * as request from 'supertest';
-import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
